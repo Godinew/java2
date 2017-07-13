57543a4e586a264481cb45ab9d38decfc6baa8ea
ваше имя < >
2017-07-13 16:10:11 +0400

    v2

---
+++ src/main/java/mavanv1/App.java
@@ -0,0 +1,13 @@
+package mavanv1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
